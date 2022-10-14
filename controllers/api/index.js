8a197500cf88f1222cdbd58d83f1289ca18ae2d0
unknown <tetiana.lisina@gmail.comgit config --global user.name T8886>
2022-10-14 14:40:55 -0500

    changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const recipeRoutes = require('./recipeRoutes')
+const favoriteRoutes = require('./favoriteRoutes')
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/favorites', favoriteRoutes)
 
 module.exports = router;
